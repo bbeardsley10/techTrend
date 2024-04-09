b61283c80212f695e17186860dd1263c40368c17
Brent Beardsley <62326878+bbeardsley10@users.noreply.github.com>
2024-04-09 18:22:18 -0500

    updates

--- signup.php
+++ signup.php
@@ -1,26 +1,26 @@
 <?php
 session_start();
 
-$con = mysqli_connect('localhost', 'root', '', 'techTrend');
+$con = mysqli_connect('localhost', 'root', 'techTrend');
 
 if (!$con) {
     echo "No Connection";
     exit; // Exit script if connection fails
 }
 
-$firstname = $_POST['first-name'];
-$lastname = $_POST['last-name'];
-$adminID = $_POST['admin-id'];
-$adminRole = $_POST['admin-role'];
+$firstname = $_POST['First_Name'];
+$lastname = $_POST['Last_Name'];
+$adminID = $_POST['Admin_ID'];
+$adminRole = $_POST['Admin_Role'];
 
-$quer = "SELECT * FROM Admin WHERE `first-name` = '$firstname' AND `last-name` = '$lastname'";
+$quer = "SELECT * FROM Admin WHERE `First_Name` = '$firstname' AND `Last_Name` = '$lastname'";
 $result = mysqli_query($con, $quer);
 $num = mysqli_num_rows($result);
 
 if ($num > 0) {
     echo "Duplicate Detected";
 } else {
-    $query = "INSERT INTO Admin (`first-name`, `last-name`, `admin-id`, `admin-role`) VALUES (?, ?, ?, ?)";
+    $query = "INSERT INTO Admin (`First_Name`, `Last_Name`, `Admin_ID`, `Admin_Role`) VALUES (?, ?, ?, ?)";
     $stmt = mysqli_prepare($con, $query);
     mysqli_stmt_bind_param($stmt, "ssss", $firstname, $lastname, $adminID, $adminRole);
     mysqli_stmt_execute($stmt);

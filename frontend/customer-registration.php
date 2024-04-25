6d213f7400259e8bc25ad91ec3b3a56221bd4d01
Brenton Beardsley <brenton.beardsley@gmail.com>
2024-04-25 17:40:53 -0500

    added some error messages for duplicate username or email

---
+++ frontend/customer-registration.php
@@ -0,0 +1,89 @@
+<?php 
+session_start();
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>techTrend Customer Registration</title>
+    <link rel="icon" href="\img\techTrendIcon.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="customer-signup-style.css">
+</head>
+<body>
+    <form action="customer-signup.php" method="post" class="container">
+        <div class="box">
+            <div class="row">
+                <div class="col-sm-5 col-xs-1 box1">
+                    <div class="inline-text">
+                        <h1>Customer Account</h1>
+                        <h1>Creation</h1>
+                    </div>
+                    <div class="user-id button2">
+                        <a href="index.php" class="btn btn-primary">Return to Main Menu</a>
+                    </div>
+                    <div class="user-id button3">
+                        <a href="customer-portal.html" class="btn btn-primary">Return to Customer Portal</a>
+                    </div>
+                    
+                </div>
+                <div class="col-sm-5 col-xs-1 box2">
+                    <div class="user-id user-data">
+                        <input type="text" name="Customer_Username" id="Customer_Username" required>
+                        <label>Enter a Username</label>
+                    </div>
+                    
+                    <div class="user-id user-data">
+                        <input type="text" name="Customer_Password" id="Customer_Password" required>
+                        <label>Enter a Password</label>
+                    </div>
+                    <div class="user-id user-data">
+                        <input type="text" name="First_Name" id="First_Name" required>
+                        <label>Enter First Name</label>
+                    </div>
+                    <div class="user-id user-data">
+                        <input type="text" name="Last_Name" id="Last_Name" required>
+                        <label>Enter Last Name</label>
+                    </div>   
+                    <div class="user-id user-data">
+                        <input type="text" name="Street_Address" id="Street_Address" required>
+                        <label>Enter Street Address</label>
+                    </div>
+                    <div class="user-id user-data">
+                        <input type="text" name="City" id="City" required>
+                        <label>Enter City</label>
+                    </div>
+                    <div class="user-id user-data">
+                        <input type="text" name="State" id="State" required>
+                        <label>Enter State</label>
+                    </div>
+                    <div class="user-id user-data">
+                        <input type="text" name="Zip_Code" id="Zip_Code" required>
+                        <label>Enter Zip Code</label>
+                    </div>
+                    <div class="user-id user-data">
+                        <input type="text" name="Customer_Email" id="Customer_Email" required>
+                        <label>Enter Email</label>
+                    </div>
+                    
+                    <div class="user-id button">
+                            <input type ="submit" name ="submitbutton" id="" value = "Create New Account"> 
+                    </div>
+                    <div class="error-messages">
+                        <?php
+                        if (isset($_SESSION['registration_error'])) {
+                            echo "<div class='user-id alert alert-danger1'>" . $_SESSION['registration_error'] . "</div>";
+                            unset($_SESSION['registration_error']);
+                        } elseif (isset($_SESSION['email_error'])) {
+                            echo "<div class='user-id alert alert-danger2'>" . $_SESSION['email_error'] . "</div>";
+                            unset($_SESSION['email_error']);
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
--- frontend/customer-signup.php
+++ frontend/customer-signup.php
@@ -12,27 +12,52 @@ $username = $_POST['Customer_Username'];
 $password = $_POST['Customer_Password'];
 $firstname = $_POST['First_Name'];
 $lastname = $_POST['Last_Name'];
+$street_address = $_POST['Street_Address'];
 $city = $_POST['City'];
 $state = $_POST['State'];
-$street_address = $_POST['Street_Address'];
 $zip_code = $_POST['Zip_Code'];
 $customer_email = $_POST['Customer_Email'];
 
 
-$quer = "SELECT * FROM Customer WHERE `First_Name` = '$firstname' AND `Last_Name` = '$lastname'";
+$quer = "SELECT * FROM Customer WHERE `Customer_Username` = '$username'";
 $result = mysqli_query($con, $quer);
 $num = mysqli_num_rows($result);
 
 if ($num > 0) {
-    echo "Duplicate Detected";
-} else {
-    $query = "INSERT INTO Customer (Customer_Username, Customer_Password, First_Name, Last_Name, City, State, Street_Address, Zip_Code, Customer_Email) VALUES (?, ?, ?,?,?,?,?,?,? )";
-    $stmt = mysqli_prepare($con, $query);
-    mysqli_stmt_bind_param($stmt, "sssssssss",$username,  $password, $firstname, $lastname, $city, $state, $street_address, $zip_code, $customer_email);
-    mysqli_stmt_execute($stmt);
-    echo "Account created successfully";
-    mysqli_stmt_close($stmt);
+    // Set session variable with error message
+    $_SESSION['registration_error'] = "The username is already taken!";
+    // Redirect back to the registration page
+    header("Location: customer-registration.php");
+    exit;
+ } 
+ 
+ 
+ // Check for duplicate email
+$email_query = "SELECT * FROM Customer WHERE Customer_Email = ?";
+$stmt = mysqli_prepare($con, $email_query);
+mysqli_stmt_bind_param($stmt, "s", $customer_email);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+$num = mysqli_num_rows($result);
+
+if ($num > 0) {
+    // Email is already linked to an account
+    $_SESSION['email_error'] = "That email is already linked to an account!";
+    header("Location: customer-registration.php");
+    exit;
 }
 
+// If no duplicates are found insert the new account
+$insert_query = "INSERT INTO Customer (Customer_Username, Customer_Password, First_Name, Last_Name, Street_Address, City, State, Zip_Code, Customer_Email) 
+                  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$stmt = mysqli_prepare($con, $insert_query);
+mysqli_stmt_bind_param($stmt, "sssssssss", $username, $password, $firstname, $lastname, $street_address, $city, $state, $zip_code, $customer_email);
+mysqli_stmt_execute($stmt);
+
+// Redirect to customer portal after successful insertion
+header("Location: customer-portal.html");
+exit; // Ensure the script stops after redirection
+
+mysqli_stmt_close($stmt);
 mysqli_close($con);
 ?>
